7d119df3b4ea17049f595c538a09fdd28b13f246
Priya_P <124154881+Priyap1038@users.noreply.github.com>
2025-05-25 19:28:22 +0530

    Integrated folder structure

--- app/api/imagekit-auth/upload/route.ts
+++ app/api/imagekit-auth/upload/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db";
 import { files } from "@/lib/db/schema";
 import { auth } from "@clerk/nextjs/server";
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -18,9 +19,37 @@ export async function POST(request: NextRequest) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    if(!imagekit || !imagekit.urlEndpoint)
+    if(!imagekit || !imagekit.urlEndpoint){
+      return NextResponse.json(
+        {error:"Invalid file upload data"},
+        {status:401}
+      );
+    }
+
+    const fileData = {
+      name: imagekit.name || "untilted",
+      path: imagekit.filePath || `/droply/${userId}/${imagekit.name}`,
+      size: imagekit.size || 0,
+      type: imagekit.fileType || "image",
+      fileUrl: imagekit.url,
+      thumbnailUrl: imagekit.thumbnailUrl || null,
+      userId: userId,
+      parentId: null, // Root level by default
+      isFolder: false,
+      isStarred: false,
+      isTrash: false,
+
+    };
+
+    const [newFile] = await db.insert(files).values(fileData).returning();
+    return NextResponse.json(newFile);
 
   } catch (error) {
-
+    return NextResponse.json(
+      {
+        error:"Failed to save info to database"
+      },
+      {status:500}
+    );
   }
 }
