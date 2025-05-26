846e88792c61950215b6f6b987ad5a3a92855944
Priya_P <124154881+Priyap1038@users.noreply.github.com>
2025-05-26 09:43:03 +0530

    added custom API endpoint for ImageKit uploads

---
+++ app/api/imagekit-auth/files/[fileId]/star/route.ts
@@ -0,0 +1,49 @@
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { error } from "console";
+import { and, eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  props: { params: Promise<{ fileId: string }> }
+) {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { fileId } = await props.params;
+
+    if (!fileId) {
+      return NextResponse.json({ error: "File is required" }, { status: 401 });
+    }
+
+    const [file] = await db
+      .select()
+      .from(files)
+      .where(and(eq(files.id, fileId), eq(files.userId, userId)));
+
+    if (file) {
+      return NextResponse.json({ error: "File not found" }, { status: 401 });
+    }
+
+    // toggle the star status
+
+    const [updatedFiles] = await db
+      .update(files)
+      .set({ isStarred: !files.isStarred })
+      .where(and(eq(files.id, fileId), eq(files.userId, userId)))
+      .returning();
+
+    // log the updated file
+    const updatedFile = updatedFiles.id;
+
+    return NextResponse.json(updatedFile);
+  } catch (error) {
+    console.log("Error starring file:", error)
+    return NextResponse.json({error:"Failed to update the file"},{status:500})
+  }
+}
